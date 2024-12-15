80f89147e350b55bf4e7d4c1b01b3e07b4538202
Ashraf <155149184+Ashraf763@users.noreply.github.com>
2024-12-15 20:56:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 
 import Login from './components/Login'
 import Wrapper from './components/Wrapper'
@@ -10,7 +10,8 @@ const App = () => (
   <Switch>
     <Route exact path="/login" component={Login} />
     <Route path="/" component={Wrapper} />
-    <Route component={NotFound} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="not-found"/>
   </Switch>
 )
 export default App
